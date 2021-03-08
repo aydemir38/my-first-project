42d40af3187b02f44b2cf3a2d088216f4f3ca0ad
aydemir38 <aydemir-ali@hotmail.com>
2021-03-08 21:59:37 +0100

    neuer commit

--- src/ControlStructurs3.java
+++ src/ControlStructurs3.java
@@ -1,2 +1,31 @@
+import java.util.Random;
+
 public class ControlStructurs3 {
+    public static void main(String[] args) {
+
+
+       int summe = 0;
+
+        boolean result = true;
+        while (result) {
+
+            Random random = new Random();
+            int randomNumber = random.nextInt( 30 - 10  ) + 10;
+
+            summe = summe + randomNumber;
+
+            System.out.println(randomNumber);
+            if (randomNumber == 15 || randomNumber == 25) {
+                result = false;
+            }
+
+
+        }
+
+        System.out.println("Programm Ende - Summe:" + summe);
+
+    }
 }
+
+
+
